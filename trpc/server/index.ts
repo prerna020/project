0b083c42c2e0c663bd55ee3642399d993bf0b341
Prerna <sprerna288@gmail.com>
2026-03-14 19:33:46 +0530

    server

--- trpc/server/index.ts
+++ trpc/server/index.ts
@@ -1,8 +1,69 @@
 import { router, publicProcedure } from "./trpc";
 import {z} from "zod";
 
-export const appRouter = router({
-    
-})
+// query -> just reads the data, can't change anything (get)
+// mutation -> can change data (post, put, delete)
 
+type Todo = {id: number, task: string, done: boolean}
+let todos: Todo[] = []
+let nextId= 1;
+
+export const appRouter = router({
+    // get all todo
+    list: publicProcedure.query(()=>{
+        return todos
+    }),
+
+    // add todo
+    add: publicProcedure
+        .input(z.object({task: z.string().min(1)})) // validate input
+        .mutation(({input}) => {
+            const todo = {id: nextId++, task: input.task, done: false};
+            todos.push(todo);
+            return todo
+        }),
+    
+    toogle: publicProcedure
+        .input(z.object({id: z.number()}))
+        .mutation(({input})=>{
+            // 1. finding todo with id == input.id
+            const todo = todos.find(t => t.id === input.id);
+            if (todo) todo.done = !todo.done;
+            return todo;
+        }),
+    
+    remove: publicProcedure
+        .input(z.object({id: z.number()}))
+        .mutation(({ input }) => {
+            todos = todos.filter(t => t.id !== input.id);
+            return { success: true };
+        })
+})
 export type AppRouter = typeof appRouter
+
+/*
+app cant send real code 
+how frontend what to endpoints exists  
+type - This creates a type-only description — like a blueprint with no actual code:
+
+
+typeof appRouter extracts THIS information:     
+                                                
+{                                               
+list:                                         
+    input: void (nothing)                       
+    output: { id: number, task: string, done: boolean }[]                 
+                                                
+add:                                          
+    input: { task: string }                     
+    output: { id: number, task: string,done: boolean }                   
+                                                
+toogle:                                       
+    input: { id: number }                       
+    output: { id: number, task: string, done: boolean } | undefined       
+                                                
+remove:                                       
+    input: { id: number }                       
+    output: { success: boolean }                
+}
+*/
