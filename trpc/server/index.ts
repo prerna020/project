2d6eb09510b6a5c050b3a73d68c5a84cc14156df
Prerna <sprerna288@gmail.com>
2026-03-18 20:54:11 +0530

    todo route

--- trpc/server/index.ts
+++ trpc/server/index.ts
@@ -1,9 +1,16 @@
 import { router, publicProcedure } from "./trpc";
-import {email, z} from "zod";
+import {z} from "zod";
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
+// @ts-ignore
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+export const SECRET = 'SECr3t';
+import cors from "cors";
+
 
 export const appRouter = router({
-    
+    user: userRouter, 
+    todo: todoRouter
 })
 
 export type AppRouter = typeof appRouter
