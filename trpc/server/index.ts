1eed9f12f93c752834d6e4c4b148344fd035cbd7
Prerna <sprerna288@gmail.com>
2026-03-19 23:53:05 +0530

    http

--- trpc/server/index.ts
+++ trpc/server/index.ts
@@ -16,3 +16,31 @@ export const appRouter = router({
 })
 
 export type AppRouter = typeof appRouter
+
+const server = createHTTPServer({
+    router: appRouter,
+    middleware: cors(),
+    createContext(opts) {
+        let authHeader = opts.req.headers["authorization"];
+
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+            console.log(token);
+            return new Promise<{db: {Todo: typeof Todo, User: typeof User}, userId?: string}>((resolve) => {
+                jwt.verify(token, SECRET, (err, user) => {
+                    if (user) {
+                        resolve({userId: user.userId as string, db: {Todo, User}});
+                    } else {
+                        resolve({db: {Todo, User}});
+                    }
+                });
+            })
+        }
+
+        return {
+            db: {Todo, User},
+        }
+    }
+});
+   
+server.listen(3000);
