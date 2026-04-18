5c4849ba0f4271ac0a1c420e969031ce63cfe4eb
Prerna <sprerna288@gmail.com>
2026-04-18 21:23:21 +0530

    chat

---
+++ chatbot/chat.py
@@ -0,0 +1,32 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langgraph.graph.message import add_messages
+
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+
+class ChatState(TypedDict): 
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+
+def chat_node(state: ChatState):
+
+    messages = state['messages']
+    response = llm.invoke(messages)
+
+    return {'messages': [response]}
+
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile()
