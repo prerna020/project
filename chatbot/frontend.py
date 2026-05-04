ebc8689604b7b1652b376dac49e82d23adf8cdba
Prerna <sprerna288@gmail.com>
2026-05-04 21:40:22 +0530

    function

--- chatbot/frontend.py
+++ chatbot/frontend.py
@@ -32,12 +32,40 @@ def loadConversation(thread_id):
 if 'msgHistory' not in st.session_state:
     st.session_state['msgHistory'] = []
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = []
+
+addThread(st.session_state['thread_id'])
+
+
 
 
 st.sidebar.title('Chatbot')
-st.sidebar.button('New Chat')
+if st.sidebar.button('New Chat'):
+    reset_chat()
 st.sidebar.header('Recents')
  
+
+for thread_id in st.session_state['chat_threads'][::-1]:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = loadConversation(thread_id)
+
+        temp_messages = []
+
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role='user'
+            else:
+                role='assistant'
+            temp_messages.append({'role': role, 'content': msg.content})
+
+        st.session_state['msgHistory'] = temp_messages
+
+
 
 # loading the conversation history
 for message in st.session_state['msgHistory']:
